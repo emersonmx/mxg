1579dc69f320e748dc79e93436fd4523de66619a
Emerson MX <emersonmx@gmail.com>
2016-12-22 12:15:30 -0300

    Add math utils

---
+++ include/mxg/math/Utils.hpp
@@ -0,0 +1,14 @@
+#ifndef MXG_MATH_UTILS_HPP_
+#define MXG_MATH_UTILS_HPP_
+
+namespace mxg {
+namespace math {
+namespace utils {
+
+constexpr const float PI{3.1415927f};
+constexpr const float PI2{PI * 2};
+
+} /* namespace utils */
+} /* namespace math */
+} /* namespace mxg */
+#endif /* MXG_MATH_UTILS_HPP_ */
