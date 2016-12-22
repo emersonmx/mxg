77ed33673a9c8e6a6f514cc07dfb04b4097d025a
Emerson MX <emersonmx@gmail.com>
2016-12-22 10:16:29 -0300

    Add Color class

---
+++ include/mxg/graphics/Color.hpp
@@ -0,0 +1,44 @@
+#ifndef MXG_GRAPHICS_COLOR_HPP_
+#define MXG_GRAPHICS_COLOR_HPP_
+
+#include <cstdint>
+
+namespace mxg {
+namespace graphics {
+
+class Color {
+    public:
+        Color();
+        Color(std::uint8_t red, std::uint8_t green, std::uint8_t blue, std::uint8_t alpha = 255);
+        explicit Color(std::uint32_t color);
+
+        std::uint32_t toInteger() const;
+
+        static const Color Black;
+        static const Color White;
+        static const Color Red;
+        static const Color Green;
+        static const Color Blue;
+        static const Color Yellow;
+        static const Color Magenta;
+        static const Color Cyan;
+        static const Color Transparent;
+
+        std::uint8_t r;
+        std::uint8_t g;
+        std::uint8_t b;
+        std::uint8_t a;
+};
+
+bool operator ==(const Color& left, const Color& right);
+bool operator !=(const Color& left, const Color& right);
+Color operator +(const Color& left, const Color& right);
+Color operator -(const Color& left, const Color& right);
+Color operator *(const Color& left, const Color& right);
+Color& operator +=(Color& left, const Color& right);
+Color& operator -=(Color& left, const Color& right);
+Color& operator *=(Color& left, const Color& right);
+
+} /* namespace graphics */
+} /* namespace mxg */
+#endif /* MXG_GRAPHICS_COLOR_HPP_ */
---
+++ test/color_test.cpp
@@ -0,0 +1,64 @@
+#include "gtest/gtest.h"
+
+#include "mxg/graphics/Color.hpp"
+
+using namespace mxg::graphics;
+
+TEST(GraphicsColorTest, CreateColor) {
+    Color c{};
+
+    ASSERT_EQ(Color::Black, c);
+}
+
+TEST(GraphicsColorTest, AssignColors) {
+    Color red = Color::Red;
+    ASSERT_EQ(Color::Red, red);
+
+    Color expectedGray{128, 128, 128};
+    Color gray{128, 128, 128};
+    ASSERT_EQ(expectedGray, gray);
+
+    // RGBA: [220, 20, 60, 255]
+    Color crimson{0xDC143CFF};
+    ASSERT_EQ(220, crimson.r);
+    ASSERT_EQ(20, crimson.g);
+    ASSERT_EQ(60, crimson.b);
+    ASSERT_EQ(255, crimson.a);
+}
+
+TEST(GraphicsColorTest, CompareColors) {
+    Color crimson{0xDC143CFF};
+    Color first = crimson;
+    Color indigo{0x4B0082FF};
+    Color second = indigo;
+    Color gray{32, 32, 32};
+    Color expected{};
+    Color aux{};
+
+    ASSERT_EQ(first, crimson);
+    ASSERT_NE(indigo, crimson);
+
+    expected = Color{0xFC345CFF};
+    ASSERT_EQ(expected, crimson + gray);
+
+    expected = Color{0xBC001C00};
+    ASSERT_EQ(expected, crimson - gray);
+
+    expected = Color{0x1B0207FF};
+    ASSERT_EQ(expected, crimson * gray);
+
+    aux = crimson;
+    aux += gray;
+    expected = Color{0xFC345CFF};
+    ASSERT_EQ(expected, aux);
+
+    aux = crimson;
+    aux -= gray;
+    expected = Color{0xBC001C00};
+    ASSERT_EQ(expected, aux);
+
+    aux = crimson;
+    aux *= gray;
+    expected = Color{0x1B0207FF};
+    ASSERT_EQ(expected, aux);
+}
