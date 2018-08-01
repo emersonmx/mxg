c1f5ea905a01113e0a08083f8ffec7c471aafe92
Emerson MX <emersonmx@gmail.com>
2018-08-01 00:04:19 -0300

    Remove Transform from Renderer

--- include/mxg/Renderer.hpp
+++ include/mxg/Renderer.hpp
@@ -1,9 +1,9 @@
 #ifndef MXG_RENDERER_HPP_
 #define MXG_RENDERER_HPP_
 
+#include <mxg/Vector2.hpp>
 #include <mxg/Color.hpp>
 #include <mxg/Texture.hpp>
-// #include <mxg/Transform.hpp>
 #include <mxg/Window.hpp>
 
 namespace mxg {
@@ -21,7 +21,11 @@ public:
     virtual void clear() = 0;
     virtual void clear(const Color& color) = 0;
     virtual void render(const Texture& texture, const Vector2& position) = 0;
-    // virtual void render(const Texture& texture, const Transform& transform) = 0;
+    virtual void render(const Texture& texture,
+                        const Vector2& position,
+                        const float angle,
+                        const Vector2& scale,
+                        const Vector2& center) = 0;
     virtual void present() = 0;
 };
 
