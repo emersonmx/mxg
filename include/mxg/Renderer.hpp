1faaa83a8e9b078cf89ee5fcdd55b153e51c78de
Emerson MX <emersonmx@gmail.com>
2018-07-31 23:54:09 -0300

    Add basic Renderer interface

---
+++ include/mxg/Renderer.hpp
@@ -0,0 +1,29 @@
+#ifndef MXG_RENDERER_HPP_
+#define MXG_RENDERER_HPP_
+
+#include <mxg/Color.hpp>
+#include <mxg/Texture.hpp>
+// #include <mxg/Transform.hpp>
+#include <mxg/Window.hpp>
+
+namespace mxg {
+
+class Renderer {
+public:
+    virtual ~Renderer() = default;
+
+    virtual Color getClearColor() const = 0;
+    virtual void setClearColor(const Color& color) = 0;
+
+    virtual void create(const Window& window) = 0;
+    virtual void destroy() = 0;
+
+    virtual void clear() = 0;
+    virtual void clear(const Color& color) = 0;
+    virtual void render(const Texture& texture, const Vector2& position) = 0;
+    // virtual void render(const Texture& texture, const Transform& transform) = 0;
+    virtual void present() = 0;
+};
+
+} /* namespace mxg */
+#endif /* MXG_RENDERER_HPP_ */
