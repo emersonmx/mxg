46133b64df0c466634234017384a763e7d0a4f01
Emerson MX <emersonmx@gmail.com>
2018-07-31 22:52:49 -0300

    Update basic_example

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -1,15 +1,18 @@
 #include <memory>
 
 #include <mxg/sdl2/Engine.hpp>
+#include <mxg/sdl2/Window.hpp>
 
 int main() {
     std::unique_ptr<mxg::Engine> engine = std::make_unique<mxg::sdl2::Engine>();
     engine->initialize();
 
-    // {
-    //     mxg::Window window;
-    //     window.create("Basic Example", {800, 600});
-    // }
+    {
+        std::unique_ptr<mxg::Window> window = std::make_unique<mxg::sdl2::Window>();
+        window->create("Basic Example", {800, 600});
+
+        SDL_Delay(2000);
+    }
 
     engine->initialize();
     return 0;
